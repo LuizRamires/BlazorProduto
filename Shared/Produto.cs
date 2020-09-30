94634a407c74f62f231fa1411d521067607bfd53
LuizRamires <luizbramires@gmail.com>
2020-09-30 16:15:19 -0300

    Erro na página

---
+++ Shared/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace blazor_mysql2.Shared
+{
+    class Produto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public string Descricao { get; set; }
+
+        [Required]
+        public double Valor { get; set; }
+    }
+}
